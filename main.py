4609e0a03395c4a4ba6cee28343863641704b9be
Vladimir <alfa.voland@gmail.com>
2020-09-03 11:02:00 +0300

    some changes....

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import datetime
 from files_functions import *
 from parsing_functions import *
 
-HOST = 'https://www.domofond.ru'
 PAGE_WITH_ALL_CITIES = 'https://www.domofond.ru/city-ratings'
 
 
 def scrap_all_data(url):
-    html_text = html_response(url)
     now_day_month_hour = datetime.datetime.today().strftime("%d.%m_%H")
     file_name = os.path.join('data', f"database_{now_day_month_hour}_hours.json")
     # file_name = os.path.join('data', f"database_22.08_17_hours.json")
 
     if not os.path.isfile(file_name):
-        write_data_in_file(parse_cities_names_and_urls(html_text), file_name)
+        html_text = html_response(url, WEB_HEADERS)
+        all_cities_names_and_urls = parse_cities_names_and_urls(html_text)
+        write_data_in_file(all_cities_names_and_urls, file_name)
         print('All cities names and URLs written into:', file_name)
     else:
         print('All cities names and URLs already into:', file_name)
--- parsing_functions.py
+++ parsing_functions.py
@@ -1,9 +1,54 @@
-import os
 import re
 import json
 import requests
 from time import sleep
 from bs4 import BeautifulSoup
+
+WEB_HEADERS = {'authority': 'www.domofond.ru', 'cache-control': 'max-age=0',
+               'upgrade-insecure-requests': '1',
+               'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWeb'
+                             'Kit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.11'
+                             '6 Safari/537.36',
+               'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,'
+                         'image/webp,image/apng,*/*;q=0.8,application/signed-'
+                         'exchange;v=b3;q=0.9',
+               'sec-fetch-site': 'none', 'sec-fetch-mode': 'navigate',
+               'sec-fetch-user': '?1', 'sec-fetch-dest': 'document',
+               'accept-language': 'en-US,en;q=0.9,ru-RU;q=0.8,ru;q=0.7'}
+API_HEADERS = {'authority': 'www.domofond.ru',
+               'cache-control': 'max-age=0',
+               'upgrade-insecure-requests': '1',
+               'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWe'
+                             'bKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.'
+                             '116 Safari/537.36',
+               'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,'
+                         'image/webp,image/apng,*/*;q=0.8,application/signed-ex'
+                         'change;v=b3;q=0.9',
+               'sec-fetch-site': 'none',
+               'sec-fetch-mode': 'navigate',
+               'sec-fetch-user': '?1',
+               'sec-fetch-dest': 'document',
+               'accept-language': 'en-US,en;q=0.9,ru-RU;q=0.8,ru;q=0.7'}
+API_URL = {'https://api.domofond.ru/rpc'}
+
+
+def html_response(url, headers, request_method='get', data=None):
+    for _ in range(3):
+        try:
+            if request_method == 'get':
+                html = requests.get(url, headers=headers)
+                return html.text
+            elif request_method == 'post':
+                html = requests.post(url, headers=headers, data=data)
+                return html.text
+            else:
+                raise Exception('You must enter post or get method')
+
+        except ConnectionError or TimeoutError:
+            print("\n*****ConnectionError or TimeoutError*****\n\n"
+                  "I will retry again after 7 seconds...")
+            sleep(7)
+            print('Making another request...')
 
 
 def parse_cities_names_and_urls(html_text):
@@ -35,7 +80,6 @@ def get_rating_from_response(file_name):
         print(str(city_number) + '/' + str(number_of_cities),
               city_name, city_data['url'])
 
-
         if 'Ecology' in city_data:
             print(f'URLs data {city_name}, is already in the cities_data')
         else:
@@ -47,86 +91,53 @@ def get_rating_from_response(file_name):
             rating_values = list()
 
             for _ in range(3):
-                try:
-                    HEADERS = {'authority': 'www.domofond.ru',
-                               'cache-control': 'max-age=0',
-                               'upgrade-insecure-requests': '1',
-                               'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-                               'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                               'sec-fetch-site': 'none',
-                               'sec-fetch-mode': 'navigate',
-                               'sec-fetch-user': '?1',
-                               'sec-fetch-dest': 'document',
-                               'accept-language': 'en-US,en;q=0.9,ru-RU;q=0.8,ru;q=0.7'}
-                    response = requests.get(city_data['url'],
-                                            headers=HEADERS)
-                    soup = BeautifulSoup(response.text, 'html.parser')
-                    ratings = soup.find_all('div',
-                                            class_="area-rating__score___3ERQc")
-                    if ratings:
-                        for rating in ratings:
-                            rating_values.append(rating.text)
-                        rating_data = dict(zip(rating_keys, rating_values))
-                        print('rating_data:', rating_data, '\n')
-                        city_data.update(rating_data)
-                        sleep(1)
-                        break
-                except:
-                    print("\n*****ConnectionError or TimeoutError*****\n\n"
-                          "I will retry again after 7 seconds...")
-                    sleep(7)
-                    print('Making another request...')
+                prices_response = html_response(city_data['url'],
+                                                WEB_HEADERS)
+                soup = BeautifulSoup(prices_response, 'html.parser')
+                ratings = soup.find_all('div',
+                                        class_="area-rating__score___3ERQc")
+                if ratings:  # page has parsed, but there is no data
+                    for rating in ratings:
+                        rating_values.append(rating.text)
+                    rating_data = dict(zip(rating_keys, rating_values))
+                    print('rating_data:', rating_data, '\n')
+                    city_data.update(rating_data)
+                    sleep(1)
+                    break
     return cities_data
 
 
 def get_prices_from_response(file_name):
     with open(file_name, encoding="utf8") as file:
-        database = json.load(file)
+        cities_data = json.load(file)
 
-    number_of_cities = len(database)
+    number_of_cities = len(cities_data)
     city_number = 0
     print('Start scraping with CURL REQUESTS', number_of_cities, 'city(-ies)\n')
 
-    for city_name, city_data in database.items():
+    for city_name, city_data in cities_data.items():
         city_number += 1
         print(str(city_number) + '/' + str(number_of_cities), city_name,
               city_data['url'])
         if 'avgScalePrice' in city_data:
-            print('CURLs data', city_name, 'is already in the database\n')
+            print('CURLs data', city_name, 'is already in the cities_data\n')
         else:
             print(f'Start scraping CURL data {city_name}...')
-            headers = {'authority': 'api.domofond.ru',
-                       'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-                       'content-type': 'text/plain', 'accept': '*/*',
-                       'origin': 'https://www.domofond.ru',
-                       'sec-fetch-site': 'same-site', 'sec-fetch-mode': 'cors',
-                       'sec-fetch-dest': 'empty',
-                       'accept-language': 'en-US,en;q=0.9,ru-RU;q=0.8,ru;q=0.7',
-                       'cookie': '_ga=GA1.2.644829252.1591689961; rrpvid=368674596745111; __gads=ID=b485e7d6f6980254:T=1591689962:S=ALNI_MZkACGlGBKzopUIdSd6-IIujZO-_A; rcuid=5de9029edf0ee900018d8c09; duid=C40ZcpV4E6VNSTpM5hRcS3cSn6ryzddpgdHxeR9JcV5DRoZF; __cfduid=dd5145dbe5ee945c165a74df8aacafd771591690026; _ym_uid=1591690029683558500; _ym_d=1591690029; _gid=GA1.2.1613467270.1594113430; _gat=1', }
+
             city_id = re.findall(r'\w+$', city_data['url'])[0][1:]
-            data = '{"id":"1","jsonrpc":"2.0","method":"priceanalysis.GetAreaPricesV1","params":{"meta":{"platform":"web","language":"ru"},"areaID":' \
-                   + city_id + ',"areaType":"City"}}'
+            data = '{"id":"1","jsonrpc":"2.0","method":"priceanalysis.GetAre' \
+                   'aPricesV1","params":{"meta":{"platform":"web","language":' \
+                   '"ru"},"areaID":' + city_id + ',"areaType":"City"}}'
 
-            while True:
-                try:
-                    response = requests.post(
-                        'https://api.domofond.ru/rpc', headers=headers,
-                        data=data)
-                    break
-                except:
-                    print("ConnectionError or TimeoutError\n"
-                          "I will retry again after 7 seconds...")
-                    sleep(7)
-                    print('Making another request...')
-
-            parsing_data = json.loads(response.text)
+            json_response = html_response(API_URL, API_HEADERS, 'post', data)
+            parsing_data = json.loads(json_response)
             prices_data = dict()
 
             def return_data_if_exist(head, *parts):
                 for part in parts:
                     try:
                         head = head[part]
-                    except:
+                    except TypeError:
                         return None
                 return head
 
@@ -166,28 +177,4 @@ def get_prices_from_response(file_name):
             print('prices_data:', prices_data, '\n')
             city_data.update(prices_data)
             sleep(1)
-    return database
-
-
-def html_response(url):
-    HEADERS = {'authority': 'www.domofond.ru', 'cache-control': 'max-age=0',
-               'upgrade-insecure-requests': '1',
-               'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-               'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-               'sec-fetch-site': 'none', 'sec-fetch-mode': 'navigate',
-               'sec-fetch-user': '?1', 'sec-fetch-dest': 'document',
-               'accept-language': 'en-US,en;q=0.9,ru-RU;q=0.8,ru;q=0.7'}
-    for _ in range(3):
-        try:
-            html = requests.get(url, headers=HEADERS)
-            print('Status code:', html.status_code)
-            return html.text
-        except ConnectionError or TimeoutError:
-            print("\n*****ConnectionError or TimeoutError*****\n\n"
-                  "I will retry again after 7 seconds...")
-            sleep(7)
-            print('Making another request...')
-        except:
-            print('Some problems with connection...')
-            sleep(7)
-            print('Making another request...')
+    return cities_data
